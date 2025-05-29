64d656645b2a75a0fb47616b4ee300314a620c47
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-05-29 18:11:11 -0500

    comit actualizado

--- src/components/shop/products/product-grid/ProductGrid.tsx
+++ src/components/shop/products/product-grid/ProductGrid.tsx
@@ -12,7 +12,7 @@ export default function ProductGrid({products}:Props) {
     <div className='grid grid-cols-2 p-2 sm:grid-cols-3 lg:grid-cols-4 gap-2'>
       {
         products.map(product => (
-          <ProductGridItem key={product.slug} product={product}/>
+          <ProductGridItem key={product.id} product={product}/>
         )
 
         )
