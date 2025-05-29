6eb819d86f83ccd5d59306eb34e1a4d33b03d449
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-05-29 18:41:07 -0500

    validando errores

--- src/components/shop/products/product-grid/ProductGrid.tsx
+++ src/components/shop/products/product-grid/ProductGrid.tsx
@@ -1,22 +1,17 @@
-import { Product } from '@/interfaces/product.interface';
-import React from 'react'
-import ProductGridItem from './ProductGridItem';
-
+import { Product } from "@/interfaces/product.interface";
+import React from "react";
+import ProductGridItem from "./ProductGridItem";
 
 interface Props {
   products: Product[];
 }
 
-export default function ProductGrid({products}:Props) {
+export default function ProductGrid({ products }: Props) {
   return (
-    <div className='grid grid-cols-2 p-2 sm:grid-cols-3 lg:grid-cols-4 gap-2'>
-      {
-        products.map(product => (
-          <ProductGridItem key={product.id} product={product}/>
-        )
-
-        )
-      }
+    <div className="grid grid-cols-2 p-2 sm:grid-cols-3 lg:grid-cols-4 gap-2">
+      {products.map((product) => (
+        <ProductGridItem key={product.id} product={product} />
+      ))}
     </div>
-  )
+  );
 }
