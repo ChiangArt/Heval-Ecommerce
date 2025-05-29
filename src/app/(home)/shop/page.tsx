c4ffbc18e5408878406ec286f59a78538ddf439e
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-05-29 18:29:35 -0500

    commit actualizado

--- src/app/(home)/shop/page.tsx
+++ src/app/(home)/shop/page.tsx
@@ -1,10 +1,11 @@
 import Filters from "@/components/shop/Filters";
 import ProductGrid from "@/components/shop/products/product-grid/ProductGrid";
 import ShopSection from "@/components/shop/ShopSection";
+import { Product } from "@/interfaces/product.interface";
 import { initialData } from "@/seed/seed";
 import React from "react";
 
-const products = initialData.products;
+const products: Product[] = initialData.products;
 
 export default function ShopPage() {
   return (
