04f7143c30572e9c7fcc090a359eee99ccd24a16
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-05-29 18:48:04 -0500

    commit actualizado ahora

--- src/app/(home)/shop/page.tsx
+++ src/app/(home)/shop/page.tsx
@@ -1,19 +1,20 @@
-// import Filters from "@/components/shop/Filters";
-// import ProductGrid from "@/components/shop/products/product-grid/ProductGrid";
-// import ShopSection from "@/components/shop/ShopSection";
-// import { Product } from "@/interfaces/product.interface";
-// import { initialData } from "@/seed/seed";
-// import React from "react";
+import Filters from "@/components/shop/Filters";
+import ProductGrid from "@/components/shop/products/product-grid/ProductGrid";
+import ShopSection from "@/components/shop/ShopSection";
+import { Product } from "@/interfaces/product.interface";
+import { initialData } from "@/seed/seed";
+import React from "react";
 
-// const products: Product[] = initialData.products;
+const products: Product[] = initialData.products;
 
-// export default function ShopPage() {
-//   return (
-//     <>
-//       <ShopSection />
-//       <Filters />
-//       <hr className="my-2 border-t-2 border-gray-300 " />
-//       <ProductGrid products={products} />
-//     </>
-//   );
-// }
+export default function ShopPageInit() {
+  return (
+    <>
+      <ShopSection />
+      <Filters />
+      <hr className="my-2 border-t-2 border-gray-300 " />
+      <hr className="my-2 border-t-2 border-gray-300 " />
+      <ProductGrid products={products} />
+    </>
+  );
+}
