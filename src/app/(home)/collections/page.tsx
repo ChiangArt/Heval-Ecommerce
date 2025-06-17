ab3ea147e9482e488a95b37578789c17ea753323
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-06-17 12:48:57 -0500

    nuevo cmmit

--- src/app/(home)/collections/page.tsx
+++ src/app/(home)/collections/page.tsx
@@ -1,21 +1,18 @@
+
 import CollectionGrid from "@/components/collections/collection-grid/CollectionGrid";
 import Pagination from "@/components/ui/pagination/Pagination";
 import { Title } from "@/components/ui/title/Title";
 import { getProducts } from "@/core/product/action/product.actions";
-import { redirect } from "next/navigation";
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { page?: string };
-}) {
-  const page = parseInt(searchParams?.page ?? "0");
 
-  const { content: products, totalPages } = await getProducts(page, 20);
 
-  if (page >= totalPages) {
-    redirect("/shop?page=0");
-  }
+
+export default async function HomePage() {
+
+
+  const { content: products, totalPages } = await getProducts(0, 20);
+
+
   return (
     <div className="p-2 md:p-5">
       <hr className="my-6 border-t-2 border-gray-300 " />
@@ -27,7 +24,7 @@ export default async function HomePage({
       />
       <hr className="my-6 border-t-2 border-gray-300 " />
       <CollectionGrid products={products} />
-      <Pagination currentPage={page} totalPages={totalPages} />
+      <Pagination currentPage={0} totalPages={totalPages} />
 
       <hr className="my-6 border-t-2 border-gray-300 " />
     </div>
