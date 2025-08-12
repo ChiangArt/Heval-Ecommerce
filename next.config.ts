445ae66a7c62ed5c1b494daea2810c2c66df1601
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-12 00:41:52 -0500

    agregando cambios

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'heval-group-rrgaeg144.s3.us-east-2.amazonaws.com', 
-        pathname: '/**',
+        protocol: "https",
+        hostname: "heval-group-rrgaeg144.s3.us-east-2.amazonaws.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'swiperjs.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "swiperjs.com",
+        pathname: "/**",
       },
     ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
 };
 
