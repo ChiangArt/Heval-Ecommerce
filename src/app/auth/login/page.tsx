517f209deba3c3b28e5a907a0683c0ce04cbcfdc
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-11 19:39:27 -0500

    agregando cambios

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import ClientLoginPage from "@/components/login/ClientLoginPage";
 
+export const dynamic = "force-dynamic";
+
+
 export default function LoginPage() {
   return (
     <Suspense fallback={<div>Cargando...</div>}>
