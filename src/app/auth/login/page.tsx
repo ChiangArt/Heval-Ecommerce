256eedf47fc20e5c55a9c7896c98e9d232ca94f5
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-11 19:25:43 -0500

    agregando el suspense en el elayout principal

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,9 @@
 import { Suspense } from "react";
 import ClientLoginPage from "@/components/login/ClientLoginPage";
 
-
 export default function LoginPage() {
-  
-
   return (
-        <Suspense fallback={<div>Cargando...</div>}>
+    <Suspense fallback={<div>Cargando...</div>}>
       <ClientLoginPage />
     </Suspense>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ToasterProvider } from "@/components/ui/toaster-provider/ToasterProvider";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,11 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body
-        className={`${inter.variable} ${jetBrainsMono.variable} font-sans`}
-      >
-        <ToasterProvider/>
-        {children}
+      <body className={`${inter.variable} ${jetBrainsMono.variable} font-sans`}>
+        <ToasterProvider />
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
