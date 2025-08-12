364f95982a3515df2c3559d9fa43fc0c7ed9676b
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-11 23:33:53 -0500

    nuevo commit

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,7 @@
-import ClientLoginPage from "@/components/login/ClientLoginPage";
+// import ClientLoginPage from "@/components/login/ClientLoginPage";
 
-export default function LoginPage({ searchParams }: { searchParams: { from?: string } }) {
-  const from = typeof searchParams.from === "string" ? searchParams.from : "/";
+// export default function LoginPage({ searchParams }: { searchParams: { from?: string } }) {
+//   const from = typeof searchParams.from === "string" ? searchParams.from : "/";
 
-  return <ClientLoginPage from={from} />;
-}
+//   return <ClientLoginPage from={from} />;
+// }
