561712ea9d07d9cf2d8f2bc9e10d2cbf5d29617b
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-12 00:33:24 -0500

    nuevo commit nuevo

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,16 @@
-import ClientLoginPage from "@/components/login/ClientLoginPage";
+// import ClientLoginPage from "@/components/login/ClientLoginPage";
 // import { Suspense } from "react";
 
+
+
 export default function LoginPage() {
-  return (
-    // <Suspense fallback={<div>Cargando...</div>}>
-      <ClientLoginPage />
-    // </Suspense>
-  );
+  return <h1>Login simple</h1>;
 }
+
+// export default function LoginPage() {
+//   return (
+//     // <Suspense fallback={<div>Cargando...</div>}>
+//       <ClientLoginPage />
+//     // </Suspense>
+//   );
+// }
