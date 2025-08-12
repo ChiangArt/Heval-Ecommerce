e117266d2f8e219504c1be7f69dc358ee17c0a62
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-11 22:41:29 -0500

    agregando cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ToasterProvider } from "@/components/ui/toaster-provider/ToasterProvider";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,8 +30,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.variable} ${jetBrainsMono.variable} font-sans`}>
-        <ToasterProvider />
-        {children}
+        <Suspense>
+          <ToasterProvider />
+          {children}
+        </Suspense>
       </body>
     </html>
   );
