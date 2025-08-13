ef2716966e469b08caa3a561e32e05fe41009f6d
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-13 12:11:45 -0500

    agregando cambios

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useEffect } from "react";
+import { useUserStore } from "@/store/user/use-auth-store";
+import { useRouter } from "next/navigation";
 import ProfileDesktop from "@/components/profile/ProfileDesktop";
 import ProfileMobile from "@/components/profile/ProfileMobile";
 
 export default function ProfilePage() {
+  const { user } = useUserStore();
+  const router = useRouter();
+
+ 
+  useEffect(() => {
+    if (!user) {
+      router.push("/auth/login");
+    }
+  }, [user, router]);
+
+  if (!user) return null; 
+
   return (
     <div className="pt-30 pb-10">
       <div className="sm:hidden">
