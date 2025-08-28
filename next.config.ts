5c4237402b3b3481f5dcf542eb3208d86506983e
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-27 21:22:45 -0500

    agregando cambios

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'heval-group-rrgaeg144.s3.us-east-2.amazonaws.com', 
-        pathname: '/**',
       },
       {
         protocol: 'https',
