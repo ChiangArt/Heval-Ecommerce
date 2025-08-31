5abbaaeeb691b47f9d6109a3a0d2d00d66578c50
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-30 21:34:32 -0500

    agregnado nuevos cambios

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,13 +2,13 @@
 
 import HomeClient from "@/components/home/client/HomeClient";
 import AnnouncementBar from "@/components/ui/announcement-bar/AnnouncementBar";
-// import TopMenu from "@/components/ui/top-menu/TopMenu";
+import TopMenu from "@/components/ui/top-menu/TopMenu";
 
 export default function HomePage() {
   return (
     <>
       <AnnouncementBar />
-      {/* <TopMenu /> */}
+      <TopMenu />
       <HomeClient />
     </>
   );
