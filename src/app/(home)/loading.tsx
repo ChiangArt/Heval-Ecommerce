05f0e2a3027b09a532ebb3d61aff10337adb9baa
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-30 23:45:28 -0500

    agregnado nuevos cambios

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -1,16 +1,9 @@
-
 export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-black via-zinc-900 to-black text-white">
-     
-    
       <div className="absolute bottom-12 text-center animate-fadeZoomIn">
-        <p className="text-lg font-semibold tracking-wider">
-          Pagina cargada
-        </p>
-        <p className="text-sm text-gray-400 mt-1">
-          Esperanos
-        </p>
+        <p className="text-lg font-semibold tracking-wider">Pagina cargada</p>
+        <p className="text-sm text-gray-400 mt-1">Esperanos</p>
       </div>
     </div>
   );
